d9062c9286ecf13df38e42f2d8a7cb7594ecb4fa
Adam Prochownik <adam@prochownik.pl>
2015-06-03 01:11:52 +0200

    Changed scripts names, added showig zlecenie in html table

--- python/zlecenia.py
+++ python/zlecenia.py
@@ -7,15 +7,14 @@ try:
     zadania = serwis.PobierzZadaniaFirmy(idFirmy)
     retVal += uniwersalne.Menu()
     retVal += uniwersalne.Zalogowany( firma )
-
-    retVal += "<table style='position:absolute;top:100px;'>"
-    for zadanie in zadania:
-        retVal += "<tr>"
-        retVal += "<td><a href=zadanie?id=" + str(zadanie.id) + ">" + str(zadanie.id) + " " + str(zadanie.data_przyjecia)  + "</a></td>"
-        retVal += "</tr>"
-    retVal += "</table>"
 except AttributeError:
     retVal += "Nie jestes zalgowowany"
-    pass
+
+retVal += "<table style='position:absolute;top:100px;'>"
+for zadanie in zadania:
+    retVal += "<tr>"
+    retVal += "<td><a href=zlecenie?id=" + str(zadanie.id) + ">" + str(zadanie.id) + " " + str(zadanie.data_przyjecia)  + "</a></td>"
+    retVal += "</tr>"
+retVal += "</table>"
 
 retVal += uniwersalne.Stopka()
--- python/zlecenie.py
+++ python/zlecenie.py
@@ -13,9 +13,14 @@ except AttributeError:
 
 if 'id' in parameters:
     zadanieId = parameters['id']
-    retVal += "<table>"
-    # zadanie = serwis.PobierzZadanieZOperacjami(zadanieId)
-    # for operacja in zadanie.operacje:
+    retVal += "<table style='position:absolute;top:100px;'>"
+    zadanie = serwis.PobierzZadanieZOperacjami(zadanieId)
+    nr_zadania = 1
+    for operacja in zadanie.operacje:
+        retVal += "<tr><td> Zadanie: " + str(nr_zadania) + "</td><td></td></tr>"
+        for powiazanieZMaszyna in operacja.powiazanieZMaszyna:
+            retVal += "<tr><td></td><td> Operacja: koszt "+ str(powiazanieZMaszyna.koszt) + ", maszyna " \
+            + str(powiazanieZMaszyna.maszyny.opis) + "<td></tr>"
         # koszt = operacja.koszt
         # maszyny = operacja.powiazanieZMaszyna.
     # for firma in serwis.PobierzWszystkieFirmy():
