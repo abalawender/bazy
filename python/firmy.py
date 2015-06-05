9814fa7142509775aae41168b178b83433f6f183
phaezah7 <phaezah7@github.com>
2015-06-05 11:40:29 +0200

    auto redirect

--- python/firmy.py
+++ python/firmy.py
@@ -6,8 +6,9 @@ if 'id' in parameters: so.idFirmy = parameters['id']
 try:
     idFirmy = so.idFirmy
     firma = serwis.PobierzFirme(idFirmy)
-    retVal += uniwersalne.Menu()
-    retVal += uniwersalne.Zalogowany( firma )
+    retVal += "<script>document.location = '/exec/zlecenia';</script>"
+    #retVal += uniwersalne.Menu()
+    #retVal += uniwersalne.Zalogowany( firma )
 except AttributeError:
     retVal += "<div style='margin-left:auto;margin-right:auto;position:relative;top:200px;border: 3px dotted red; background-color:rgba(250,60,10,.1); border-radius: 25px;padding:50px; width:400px; text-align:center;'>"
     retVal += "Nie jesteś zalogowany! Wybierz użytkownika:<br /><br /><select onChange='window.location.href=this.value' style='width:80%; border-radius: 5px; border: 1px solid black; background-color:rgba(250,250,250,.2); padding: 5px;'>\n"
